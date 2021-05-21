01ab9c6ff914a815561220bfdf5ff84acbe9acda
lenaKopito <lenchisk@gmail.com>
2021-05-21 22:54:06 +0300

    Completed module

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -4,7 +4,7 @@ public class Utilities {
     public static long getLongValue(String in) {
         Long out = Long.MIN_VALUE;
         try {
-            Long.parseLong(in);
+            out = Long.parseLong(in);
         } catch (NumberFormatException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(in + "cannot be converted into a 'long' value. Exiting program.");
@@ -13,9 +13,9 @@ public class Utilities {
     }
 
     public static int getIntValue(String in) {
-         int out = Integer.MIN_VALUE;
+        int out = Integer.MIN_VALUE;
         try {
-            Integer.parseInt(in);
+            out = Integer.parseInt(in);
         } catch (NumberFormatException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(in + "cannot be converted into a 'int' value. Exiting program.");
@@ -26,7 +26,7 @@ public class Utilities {
     public static float getFloatValue(String in) {
         float out = Float.MIN_VALUE;
         try {
-            Float.parseFloat(in);
+            out = Float.parseFloat(in);
         } catch (NumberFormatException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(in + "cannot be converted into a 'float' value. Exiting program.");
